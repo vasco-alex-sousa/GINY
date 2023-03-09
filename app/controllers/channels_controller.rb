968f470de9a622f7a87a07d6067dbbae47700a25
Fred Matias <fredrmatias@gmail.com>
2023-03-09 12:47:43 +0000

    channel/id

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,7 +1,10 @@
 class ChannelsController < ApplicationController
-  def index
-    @user = current_user
-    @user_id = current_user.id
+  def show
+    @user = User.find(params[:id])
+    category_count = @user.streams.where("created_at >= ?", 30.days.ago).group(:category_id).count
+    @top_categories = category_count.sort_by { |_, count| -count }.take(3).map(&:first)
+    @user_clips = Clip.includes(stream: :user).where(stream: { user: @user })
+
     @num_streams = params[:num_streams] || 5
     @streams = @user.last_streams(@num_streams)
   end
