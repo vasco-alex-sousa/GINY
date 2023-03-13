8a0989a96cefeff463d30ce3c4d23d5d47614769
Tiago Pinto <jotipinto@gmail.com>
2023-03-13 16:36:46 +0000

    search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,10 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def search
+    @query = params[:query]
+    @categories = Category.where("lower(name) LIKE ?", "%#{@query.downcase if !@query.nil?}%") || []
+    @channels = User.where("lower(display_name) LIKE ?", "%#{@query.downcase if !@query.nil?}%")
+  end
 end
