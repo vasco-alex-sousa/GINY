d776f6b29039d8bd4d9fba5837a6baba74691fea
Fred Matias <fredrmatias@gmail.com>
2023-03-13 18:20:01 +0000

    swiper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ class PagesController < ApplicationController
   def home
     @user = current_user
     @streams = Stream.all
-    @categories = Category.all
+    @all_categories = Category.all
     @clips = Clip.all
 
-    @last_week_top_categories = @categories.sort_by { |category| -category.average_viewers }.take(5)
+    @last_week_top_categories = @all_categories.sort_by { |category| -category.average_viewers }.take(5)
     @last_week_top_channels = @streams.sort_by { |stream| -stream.average_viewers }.take(5).map(&:user).uniq
     @last_week_top_categories_clips = @last_week_top_categories.map { |category| category.clips.order("view_count DESC").first }
     @last_week_top_channels_clips = @last_week_top_channels.map { |user| user.clips.order("view_count DESC").first }
