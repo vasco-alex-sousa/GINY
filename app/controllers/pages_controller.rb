4b9bfb19a0b9bc0117ef21e239d28d35677b067d
Fred Matias <fredrmatias@gmail.com>
2023-03-14 17:55:21 +0000

    home,category,swiper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,10 +11,17 @@ class PagesController < ApplicationController
     @last_week_top_channels_clips = @last_week_top_channels.map { |user| user.clips.order("view_count DESC").first }
 
     @categories = Category.order(average_viewers: :desc).limit(5)
-    @channels = User.joins(:streams).select("users.*, AVG(streams.average_viewers) as average_viewers").group("users.id").order("average_viewers desc").limit(5)
+    @channels = User.joins(:streams).select("users.*, MAX(streams.viewer_count) as viewer_count").group("users.id").order("viewer_count desc").limit(10)
+
   end
 
   def discover
     @users = User.where.not(id: current_user.id).order("RANDOM()").limit(30)
   end
+
+  def upcoming
+    @channels = User.joins(:streams).select("users.*, MAX(streams.viewer_count) / users.follower_count AS performance_score")
+                    .group("users.id").order("performance_score DESC").limit(10)
+  end
+
 end
