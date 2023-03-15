f0138457a9e449c9e59baf17cbbe5ca636985c9a
Skullfiredevil <97984792+Skullfiredevil@users.noreply.github.com>
2023-03-15 17:20:17 +0000

    Revert "Revert "font and background styling""

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,3 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
